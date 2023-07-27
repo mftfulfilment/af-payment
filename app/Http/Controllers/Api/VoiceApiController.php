de949e55070a8225e510f2fa47c202fc2fe78cb0
douglasrono <douglasronno@gmail.com>
2023-07-27 16:46:57 +0300

    random commits

--- app/Http/Controllers/Api/VoiceApiController.php
+++ app/Http/Controllers/Api/VoiceApiController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class VoiceApiController extends Controller
 {
@@ -16,31 +17,27 @@ class VoiceApiController extends Controller
         $isActive = $request->isActive;
         $direction = $request->direction;
         $callerNumber = $request->callerNumber;
+        $sessionId = $request->sessionId;
         $destinationNumber = $request->destinationNumber;
         $dialedNumber = '254110666140';
         $dtmfDigits = $request->dtmfDigits;
 
-
         if ($isActive == 1) {
             if ($direction == 'Inbound') {
 
-                $welcome_text = "Welcome to JDF Organisation Limited.";
-                $promp_action = "To speak to an agent press 1. To register on JDF account press 2. To exit press 0";
-
+                $welcome_text = "Welcome to JDF Organisation.";
+                $prompt_action = "To speak to one of our call representative  press 1. To register a JDF account press 2.To contribute to an organisation press 3.To exit press 0";
 
                 $response = '<?xml version="1.0" encoding="UTF-8"?>';
                 $response .= '<Response>';
                 $response .= '<Say voice="en-US-Wavenet-F">' . $welcome_text . '</Say>';
-                $response .= '<Say voice="en-US-Wavenet-F">' . $promp_action . '</Say>';
-
-                $response .= '<Record trimSilence="true"></Record>';
+                $response .= '<Say voice="en-US-Wavenet-F">' . $prompt_action . '</Say>';
                 $response .= '</Response>';
                 header('Content-type: application/xml');
                 echo $response;
                 exit();
 
-
-                if($dtmfDigits == 1){
+                if ($dtmfDigits == 1) {
                     $connect_text = "Please wait while we transfer your call to the next available agent.This call may be recorded for internal training and quality purposes.";
                     $response = '<?xml version="1.0" encoding="UTF-8"?>';
                     $response .= '<Response>';
@@ -51,11 +48,8 @@ class VoiceApiController extends Controller
                     echo $response;
                     exit();
 
-
-                }
-
-               else if($dtmfDigits == 2){
-                   $get_number_text = "Please enter your phone number in the international format for us to create an account for you.";
+                } else if ($dtmfDigits == 2) {
+                    $get_number_text = "Please enter your phone number in the international format for us to create an account for you.";
 
                     $response = '<?xml version="1.0" encoding="UTF-8"?>';
                     $response .= '<Response>';
@@ -66,9 +60,20 @@ class VoiceApiController extends Controller
                     echo $response;
                     exit();
 
-                }
+                }else if ($dtmfDigits == 3) {
 
-                else{
+                    $get_amount_text = "Please enter the dontation amount.";
+
+                    $response = '<?xml version="1.0" encoding="UTF-8"?>';
+                    $response .= '<Response>';
+                    $response .= '<Say voice="en-US-Wavenet-F">' . $get_amount_text . '</Say>';
+                    $response .= '<Record trimSilence="true"></Record>';
+                    $response .= '</Response>';
+                    header('Content-type: application/xml');
+                    echo $response;
+                    exit();
+
+                } else {
                     $goodbye_text = "Thank you for calling JDF organisation.Until next time it is a Goodbye";
 
                     $response = '<?xml version="1.0" encoding="UTF-8"?>';
@@ -93,30 +98,81 @@ class VoiceApiController extends Controller
 
             }
 
-        } else {
+        }else {
+
+            $recordingUrl = $request->recordingUrl;
+            $durationInSeconds = $request->durationInSeconds;
+            $currencyCode = $request->currencyCode;
+            $amount = $request->amount;
+            $hangupCause = $request->hangupCause;
+
+            $call_history = DB::table('call_histories')
+                ->where('sessionId', $sessionId)
+                ->first();
+
+            if($call_history){
+
+                $update_call_history = DB::table('call_histories')
+                    ->where('sessionId', $sessionId)
+                    ->update([
+                        'isActive' => $isActive,
+                        'recordingUrl' => $recordingUrl,
+                        'durationInSeconds' => $durationInSeconds,
+                        'currencyCode' => $currencyCode,
+                        'amount' => $amount,
+                        'hangupCause' => $hangupCause,
+                        'updated_at' => date('Y-m-d H:i:s'),
+                    ]);
+
+
+                    DB::table('call_agents')
+                    ->where('sessionId', $sessionId)
+                    ->update([
+                        'status' => 'available',
+                        'sessionId' => null,
+                        'updated_at' => date('Y-m-d H:i:s'),
+                    ]);
+
+            }
+
 
         }
 
     }
 
-    public function handleEvent()
+    public function handleEvent(Request $request)
     {
-        //
+        $event = $request->input('eventType');
+
+
+        if ($event === 'DtmfReceived') {
+            $dtmfDigits = $request->input('dtmfDigits');
+
+
+            if ($dtmfDigits === '1') {
+
+                $dialedNumber = '254110666140';
+                $connect_text = "Please wait while we transfer your call to the next available agent. This call may be recorded for internal training and quality purposes.";
+
+                $response = '<?xml version="1.0" encoding="UTF-8"?>';
+                $response .= '<Response>';
+                $response .= '<Say voice="en-US-Wavenet-F">' . $connect_text . '</Say>';
+                $response .= '<Dial record="true" sequential="true" phoneNumbers="' . $dialedNumber . '"/>';
+                $response .= '</Response>';
+                header('Content-type: application/xml');
+                echo $response;
+                exit();
+            }
+
+
+        }
+
+
+        return response()->json(['status' => 'success']);
     }
 
-    public function transferCall(Request $request)
-    {
-        //
-    }
 
-    public function dequeueCall(string $id)
-    {
-        //
-    }
 
-    public function generateToken(string $id)
-    {
-        //
-    }
+
 
 }
